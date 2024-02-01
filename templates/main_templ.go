3bde5500079721f349c58459e083d08df55f8d2d
felipefbs <me@felipefbs.dev>
2024-02-01 13:47:28 -0300

    refactor header

--- templates/main_templ.go
+++ templates/main_templ.go
@@ -27,7 +27,39 @@ func Main() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"pt-BR\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" type=\"text/css\" href=\"/static/reset.css\"><script src=\"https://unpkg.com/htmx.org@1.9.10\" integrity=\"sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC\" crossorigin=\"anonymous\"></script><script src=\"https://unpkg.com/htmx.org/dist/ext/json-enc.js\"></script><title>Ranços</title></head><body><header><nav><button hx-get=\"/ick-list\" hx-target=\"#main-container\">Listagem</button> <button hx-get=\"/register\" hx-target=\"#main-container\">Cadastre seus ranços</button> <button hx-get=\"/definition\" hx-target=\"#main-container\">O que é?</button> <button hx-get=\"/register-user\" hx-target=\"#main-container\">Cadastrar ou Logar</button> <button hx-post=\"/logout\" hx-target=\"#main-container\">Deslogar</button></nav></header><main id=\"main-container\"><div hx-get=\"/ick-list\" hx-target=\"#main-container\" hx-trigger=\"load\"></div></main></body></html>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"pt-BR\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" type=\"text/css\" href=\"/static/reset.css\"><script src=\"https://unpkg.com/htmx.org@1.9.10\" integrity=\"sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC\" crossorigin=\"anonymous\"></script><script src=\"https://unpkg.com/htmx.org/dist/ext/json-enc.js\"></script><title>Ranços</title></head><body>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = Header().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<main id=\"main-container\"><div hx-get=\"/ick-list\" hx-target=\"#main-container\" hx-trigger=\"load\"></div></main></body></html>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		if !templ_7745c5c3_IsBuffer {
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteTo(templ_7745c5c3_W)
+		}
+		return templ_7745c5c3_Err
+	})
+}
+
+func Header() templ.Component {
+	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
+		if !templ_7745c5c3_IsBuffer {
+			templ_7745c5c3_Buffer = templ.GetBuffer()
+			defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var2 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var2 == nil {
+			templ_7745c5c3_Var2 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<header><nav><button hx-get=\"/ick-list\" hx-target=\"#main-container\">Listagem</button> <button hx-get=\"/register\" hx-target=\"#main-container\">Cadastre seus ranços</button> <button hx-get=\"/definition\" hx-target=\"#main-container\">O que é?</button> <button hx-get=\"/register-user\" hx-target=\"#main-container\">Cadastrar ou Logar</button> <button hx-post=\"/logout\" hx-target=\"#main-container\">Deslogar</button></nav></header>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -46,9 +78,9 @@ func RegisterIck() templ.Component {
 			defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 		}
 		ctx = templ.InitializeContext(ctx)
-		templ_7745c5c3_Var2 := templ.GetChildren(ctx)
-		if templ_7745c5c3_Var2 == nil {
-			templ_7745c5c3_Var2 = templ.NopComponent
+		templ_7745c5c3_Var3 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var3 == nil {
+			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<h1>Cadastre seus ranços</h1><form><input placeholder=\"qual o ranço da vez?\" type=\"text\" name=\"ick\"> <button type=\"button\" hx-post=\"/register\" hx-target=\"#main-container\">eca!</button></form>")
@@ -70,9 +102,9 @@ func RegisterUser() templ.Component {
 			defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 		}
 		ctx = templ.InitializeContext(ctx)
-		templ_7745c5c3_Var3 := templ.GetChildren(ctx)
-		if templ_7745c5c3_Var3 == nil {
-			templ_7745c5c3_Var3 = templ.NopComponent
+		templ_7745c5c3_Var4 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var4 == nil {
+			templ_7745c5c3_Var4 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div><h1>Login</h1><form><input placeholder=\"username\" type=\"text\" name=\"username\"> <input placeholder=\"senha\" type=\"password\" name=\"password\"> <button type=\"button\" hx-post=\"/login\" hx-target=\"#main-container\" hx-ext=\"json-enc\">login</button></form></div><div><h1>Cadastre-se</h1><p>Seus dados serão usados apenas para fazer uma simples analise. Prometo não vender nada. Se quiser conferir o código só acessar o projeto <a href=\"www.github.com/felipefbs/ick-app\">aqui</a>.</p><form><input placeholder=\"username\" type=\"text\" name=\"username\"> <input placeholder=\"Seu Nome\" type=\"text\" name=\"name\"> <input placeholder=\"Data de Nascimento\" type=\"date\" name=\"birthdate\"> <input placeholder=\"Genero\" type=\"text\" name=\"gender\"> <input placeholder=\"senha\" type=\"password\" name=\"password\"> <button type=\"button\" hx-post=\"/register-user\" hx-target=\"#main-container\" hx-ext=\"json-enc\">Cadastrar</button></form></div>")
@@ -94,9 +126,9 @@ func Definition() templ.Component {
 			defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 		}
 		ctx = templ.InitializeContext(ctx)
-		templ_7745c5c3_Var4 := templ.GetChildren(ctx)
-		if templ_7745c5c3_Var4 == nil {
-			templ_7745c5c3_Var4 = templ.NopComponent
+		templ_7745c5c3_Var5 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var5 == nil {
+			templ_7745c5c3_Var5 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<h1>Icks</h1><p>Icks, em uma tradução não informal, é um ranço que você tenha, geralmente totalmente aleatorio e irracional, mas as vezes assertivo e podendo ser algo que afete completamente o seu relacionamento. </p><p>Ele geralmente vem no seu primeiro encontro, onde seu pretendente faz algo que para ele pode ser até normal, mas você tem um ranço profundo sobre isso.</p><ul><li>Dentes levemente tortos</li><li>Mastiga de boca aberta</li><li>Fica com bafo de suco de abacaxi depois de tomar suco de abacaxi</li></ul>")
@@ -118,9 +150,9 @@ func IckList(ickList []entities.Ick, loggedUsername string, upvotedList map[uuid
 			defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 		}
 		ctx = templ.InitializeContext(ctx)
-		templ_7745c5c3_Var5 := templ.GetChildren(ctx)
-		if templ_7745c5c3_Var5 == nil {
-			templ_7745c5c3_Var5 = templ.NopComponent
+		templ_7745c5c3_Var6 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var6 == nil {
+			templ_7745c5c3_Var6 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<ul hx-get=\"/ick-list\" hx-swap=\"outerHTML\">")
@@ -132,12 +164,12 @@ func IckList(ickList []entities.Ick, loggedUsername string, upvotedList map[uuid
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			var templ_7745c5c3_Var6 string
-			templ_7745c5c3_Var6, templ_7745c5c3_Err = templ.JoinStringErrs(item.Ick)
+			var templ_7745c5c3_Var7 string
+			templ_7745c5c3_Var7, templ_7745c5c3_Err = templ.JoinStringErrs(item.Ick)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `main.templ`, Line: 86, Col: 14}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `main.templ`, Line: 90, Col: 14}
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var6))
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var7))
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -145,12 +177,12 @@ func IckList(ickList []entities.Ick, loggedUsername string, upvotedList map[uuid
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			var templ_7745c5c3_Var7 string
-			templ_7745c5c3_Var7, templ_7745c5c3_Err = templ.JoinStringErrs(item.User.Username)
+			var templ_7745c5c3_Var8 string
+			templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs(item.User.Username)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `main.templ`, Line: 86, Col: 39}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `main.templ`, Line: 90, Col: 39}
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var7))
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
