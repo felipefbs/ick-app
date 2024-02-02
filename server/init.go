70737de2550c1083a22a923f3ac288cd675787dd
felipefbs <me@felipefbs.dev>
2024-02-01 21:56:20 -0300

    verify cookie session

--- server/init.go
+++ server/init.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -12,9 +14,13 @@ func Init(db *sql.DB) *http.Server {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger)
+	router.Use(VerifyCookieSession)
 
-	registerIckRoutes(router, db)
+	fs := http.FileServer(http.Dir("./static"))
+	router.Handle("/static/*", http.StripPrefix("/static/", fs))
+
 	registerUserRoutes(router, db)
+	registerIckRoutes(router, db)
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -22,4 +28,31 @@ func Init(db *sql.DB) *http.Server {
 	}
 
 	return server
+}
+
+func VerifyCookieSession(next http.Handler) http.Handler {
+	f := func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/register-user" || r.URL.Path == "/login" {
+			fmt.Println(r.URL.Path)
+			next.ServeHTTP(w, r)
+
+			return
+		}
+
+		coo, err := r.Cookie("session-cookie")
+		if err != nil || coo.Valid() != nil {
+			http.Redirect(w, r, "/register-user", http.StatusPermanentRedirect)
+			next.ServeHTTP(w, r)
+
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "user", coo.Value)
+
+		r = r.WithContext(ctx)
+
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(f)
 }
